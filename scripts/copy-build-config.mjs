1c2d8a8c61e1e99c3af70a8db5f8185e16074cdb
Alexander Berl <a.berl@outlook.com>
2026-06-10 00:07:50 +0200

    Add back sources to package

--- scripts/copy-build-config.mjs
+++ scripts/copy-build-config.mjs
@@ -3,6 +3,7 @@ import path from 'node:path';
 
 const sourcePath = path.resolve('eventstore.config.json');
 const targetPath = path.resolve('build/eventstore.config.json');
+
 fs.mkdirSync(path.dirname(targetPath), { recursive: true });
 fs.copyFileSync(sourcePath, targetPath);
 
